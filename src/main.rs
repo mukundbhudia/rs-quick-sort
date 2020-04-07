d595937c4a82435255499b60e2986c4c9ce6385d
Mukund Bhudia <mukund_bhudia@hotmail.com>
2020-04-07 17:10:22 +0100

    Use of array instead of vector

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
     println!("Hello, world!");
-    assert_eq!(quick_sort(&mut vec![3,2,1,6,5,4,7]), vec![1,2,3,4,5,6,7]);
+    // assert_eq!(quick_sort(vec![3,2,1,6,5,4,7]), vec![1,2,3,4,5,6,7]);
 }
 
-fn partition(array_to_sort: &mut Vec<usize>, low: usize, high: usize) -> usize {
+fn partition(array_to_sort: &mut [isize], low: usize, high: usize) -> usize {
     let pivot = array_to_sort[high];
 
     let mut i = low;
@@ -24,27 +24,24 @@ fn partition(array_to_sort: &mut Vec<usize>, low: usize, high: usize) -> usize {
     i
 }
 
-fn qs(array_to_sort: &mut Vec<usize>, low: usize, high: usize) {
+fn qs(array_to_sort: &mut [isize], low: usize, high: usize) {
     // We may want to use `std::cmp::Ord` here later (https://doc.rust-lang.org/std/cmp/trait.Ord.html)
     if low < high {
         let mid = partition(array_to_sort, low, high);
         qs(array_to_sort, low, mid);
         qs(array_to_sort, mid + 1, high);
     }
+    println!("qs: {:?}", array_to_sort);
 }
 
-pub fn quick_sort(array_to_sort: &mut Vec<usize>) -> Vec<usize> {
-    let mut output = array_to_sort;
-    let pivot = output.len()/2;
+pub fn quick_sort(array_to_sort: &mut [isize]) {
+    // let pivot = array_to_sort.len()/2;
     let low = 0;
-    let high = output.len();
-    // let value_at_pivot = array_to_sort[pivot];
+    let high = array_to_sort.len();
+    let mut output = array_to_sort;
+    qs(&mut output, high, low);
 
-    partition(output, high, low);
-    qs(output, low, pivot);
-    qs(output, pivot + 1, high);
-
-    output.to_vec()
+    // output.to_vec()
 }
 
 #[cfg(test)]
@@ -58,16 +55,29 @@ mod tests {
 
     #[test]
     fn basic_sort_1() {
-        assert_eq!(quick_sort(&mut vec![1,2,3]), vec![1,2,3]);
+        let mut array = [1,2,3];
+        quick_sort(&mut array);
+        assert_eq!(array, [1,2,3]);
     }
 
     #[test]
     fn basic_sort_2() {
-        assert_eq!(quick_sort(&mut vec![3,2,1]), vec![1,2,3]);
+        let mut array = [3,2,1];
+        qs(&mut array,0,2);
+        assert_eq!(array, [1,2,3]);
     }
 
     #[test]
     fn basic_sort_3() {
-        assert_eq!(quick_sort(&mut vec![16,9,4,6,12,3,8,7]), vec![3,4,6,7,8,9,12,16]);
+        let mut array = [16,9,4,6,12,3,8,7];
+        qs(&mut array,0,7);
+        assert_eq!(array, [3,4,6,7,8,9,12,16]);
+    }
+
+    #[test]
+    fn basic_sort_4() {
+        let mut array = [3, 5, 1, 4, 2];
+        qs(&mut array,0,4);
+        assert_eq!(array, [1, 2, 3, 4, 5]);
     }
 }
