ab6f26076f2d9d28951fc95c5e6b1682d8bd96c2
Mukund Bhudia <mukund_bhudia@hotmail.com>
2020-04-07 13:22:13 +0100

    Added basic implementation of Hoare partition scheme

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,50 @@
 fn main() {
     println!("Hello, world!");
-    assert_eq!(quick_sort(vec![3,2,1,6,5,4,7]), vec![1,2,3,4,5,6,7]);
+    assert_eq!(quick_sort(&mut vec![3,2,1,6,5,4,7]), vec![1,2,3,4,5,6,7]);
 }
 
-fn partition(array_to_sort: &Vec<usize>, high: usize, low: usize) -> usize {
-    0
+fn partition(array_to_sort: &mut Vec<usize>, low: usize, high: usize) -> usize {
+    let pivot = array_to_sort[high];
+
+    let mut i = low;
+    let mut j = low;
+
+    while j < high - 1 {
+        if array_to_sort[j] < pivot {
+            array_to_sort.swap(i, j);
+            i += 1;
+        }
+        j += 1;
+    }
+
+    if array_to_sort[high] < array_to_sort[i] {
+        array_to_sort.swap(i, high);
+    }
+
+    i
 }
 
-fn qs(array_to_sort: &Vec<usize>, high: usize, low: usize) {
+fn qs(array_to_sort: &mut Vec<usize>, low: usize, high: usize) {
+    // We may want to use `std::cmp::Ord` here later (https://doc.rust-lang.org/std/cmp/trait.Ord.html)
     if low < high {
-        qs(array_to_sort, high, low);
+        let mid = partition(array_to_sort, low, high);
+        qs(array_to_sort, low, mid);
+        qs(array_to_sort, mid + 1, high);
     }
 }
 
-pub fn quick_sort(array_to_sort: Vec<usize>) -> Vec<usize> {
-    let pivot = array_to_sort.len()/2;
+pub fn quick_sort(array_to_sort: &mut Vec<usize>) -> Vec<usize> {
+    let mut output = array_to_sort;
+    let pivot = output.len()/2;
     let low = 0;
-    let high = array_to_sort.len();
-    let value_at_pivot = array_to_sort[pivot];
+    let high = output.len();
+    // let value_at_pivot = array_to_sort[pivot];
 
-    partition(&array_to_sort, high, low);
-    qs(&array_to_sort, low, pivot);
-    qs(&array_to_sort, pivot + 1, high);
+    partition(output, high, low);
+    qs(output, low, pivot);
+    qs(output, pivot + 1, high);
 
-    array_to_sort
+    output.to_vec()
 }
 
 #[cfg(test)]
@@ -37,16 +58,16 @@ mod tests {
 
     #[test]
     fn basic_sort_1() {
-        assert_eq!(quick_sort(vec![1,2,3]), vec![1,2,3]);
+        assert_eq!(quick_sort(&mut vec![1,2,3]), vec![1,2,3]);
     }
 
     #[test]
     fn basic_sort_2() {
-        assert_eq!(quick_sort(vec![3,2,1]), vec![1,2,3]);
+        assert_eq!(quick_sort(&mut vec![3,2,1]), vec![1,2,3]);
     }
 
     #[test]
     fn basic_sort_3() {
-        assert_eq!(quick_sort(vec![16,9,4,6,12,3,8,7]), vec![3,4,6,7,8,9,12,16]);
+        assert_eq!(quick_sort(&mut vec![16,9,4,6,12,3,8,7]), vec![3,4,6,7,8,9,12,16]);
     }
 }
